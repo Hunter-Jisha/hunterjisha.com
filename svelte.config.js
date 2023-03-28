38da8368de36024bb5c5390b5e60c135cf3893a7
Hunter Jisha <hjis3001@7-11.com>
2023-03-28 13:02:24 -0500

    added tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -15,7 +16,8 @@ const config = {
 			precompress: false,
 			strict: true
 		  })
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
