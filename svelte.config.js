fda106cf31b2439d46e86a1bdec2c9582f941090
hunter-jisha <hunterjisha@gmail.com>
2023-05-11 22:46:22 -0500

    build fix

--- svelte.config.js
+++ svelte.config.js
@@ -15,10 +15,7 @@ const config = {
                 precompress: false,
                 strict: true
             }
-        ),
-        prerender: {
-            default: true
-        }
+        )
 	},
     preprocess: vitePreprocess()
 };
